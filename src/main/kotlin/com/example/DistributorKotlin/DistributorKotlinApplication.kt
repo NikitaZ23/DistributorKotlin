d57fab5f86fa87b0150ba2eb8107eb9a9f69e281
nikita <nik_z12@mail.ru>
2024-08-04 21:42:42 +0300

    create project

---
+++ src/main/kotlin/com/example/DistributorKotlin/DistributorKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.example.DistributorKotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DistributorKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<DistributorKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/DistributorKotlin/DistributorKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.DistributorKotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DistributorKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
